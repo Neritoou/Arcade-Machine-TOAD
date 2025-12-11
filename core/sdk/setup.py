a4b5d47e80412f6501a3fb630dc582b98c8c68fc
Neritoou <agdsds245@gmail.com>
2025-12-11 02:06:48 -0400

    Adaptando datos

--- core/sdk/setup.py
+++ core/sdk/setup.py
@@ -6,4 +6,15 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
+    description="SDK oficial para Arcade Machine",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Neritoou",
+    url="https://github.com/Neritoou/arcade-machine-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
 )
