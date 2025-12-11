a01ac60f9dd85bbcbf1d1b9421688443a80883f2
Neritoou <agdsds245@gmail.com>
2025-12-11 02:14:43 -0400

    Pruebas

---
+++ games/ExampleTemplate/src/game/asd.py
@@ -0,0 +1,5 @@
+from arcade_machine_sdk.screen import Screen, ScreenContext
+from arcade_machine_sdk.interfaces.game_module import GameModule
+from arcade_machine_sdk.constants import SOME_CONSTANT
+
+print("SDK importado correctamente")
